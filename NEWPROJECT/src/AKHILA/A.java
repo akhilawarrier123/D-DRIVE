ccb3444981036fe4e4f106da55e3af817d939ceb
akhilawarrier123 <akhilawarrier5@gmail.com>
2022-09-13 11:25:06 +0530

    A&K

---
+++ NEWPROJECT/src/AKHILA/A.java
@@ -0,0 +1,14 @@
+package AKHILA;
+
+public class A {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("HAPPY SUNDAY");
+		int a=45;
+		System.out.println(a);
+		
+
+	}
+
+}
---
+++ NEWPROJECT/src/AKHILA/K.java
@@ -0,0 +1,15 @@
+package AKHILA;
+
+public class K {
+
+	public static void main(String[] args) 
+	{
+	int a=4;
+	int b=6;
+	int c=a+b;
+	System.out.println(c);
+	
+
+	}
+
+}
